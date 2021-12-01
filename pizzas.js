de1d3ffc6835e0e38d5a4c7bca0a337141843f5a
Matheus Moura <matheuslnmoura@gmail.com>
2021-11-30 22:08:59 -0300

    Projeto Finalizado

---
+++ pizzas.js
@@ -0,0 +1,10 @@
+let pizzaJson = [
+    {id:0, name:'Mussarela', img:'images/pizza.png', price:[20.00, 27.50, 35.00], sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:1, name:'Calabresa', img:'images/pizza2.png', price:[22.00, 27.50, 35.00], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Quatro Queijos', img:'images/pizza3.png', price:[22.00, 27.50, 35.00], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Americana', img:'images/pizza4.png', price:[25.00, 32.50, 38.00], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Sorvete', img:'images/pizza5.png', price:[20.00, 27.50, 35.00], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Moda da Casa', img:'images/pizza6.png', price:[26.50, 33.50, 41.00], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Chocolate', img:'images/pizza7.png', price:[15.00, 22.50, 27.00], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+];
+
